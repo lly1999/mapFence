b96c6f5d48f976dedbbdd86399bd41c1f40c20a5
v-qingyuanyige <2857092231@qq.com>
2022-09-23 16:56:09 +0800

    "v0.1 项目初始化"
    新建springboot项目，配置数据库信息

---
+++ src/main/java/com/example/mapfence/MapFenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapfence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapFenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapFenceApplication.class, args);
+    }
+
+}
