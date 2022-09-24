2bd285540913d921ce28eee4a10a4ab57e497f92
v-qingyuanyige <2857092231@qq.com>
2022-09-24 17:16:07 +0800

    “v0.3 项目初始化"
    新增CodeGenerator

---
+++ src/main/java/com/example/mapfence/utils/CodeGenerator.java
@@ -0,0 +1,42 @@
+package com.example.mapfence.utils;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
+
+/**
+ * mp代码生成器
+ * by 张渝
+ * @since 2022-09-24
+ */
+public class CodeGenerator {
+
+    static String proj_dir = System.getProperty("user.dir");
+
+    public static void main(String[] args) {
+        generate();
+    }
+
+    private static void generate() {
+        FastAutoGenerator.create("jdbc:mysql://101.37.246.72:3306/map?serverTimezone=GMT%2b8", "root", "jinNIU123456!")
+                .globalConfig(builder -> {
+                    builder.author("xavi") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir(proj_dir + "\\src\\main\\java\\"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.example.mapfence") // 设置父包名
+                            .moduleName("") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, proj_dir + "\\src\\main\\resources\\mapper\\")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("user") // 设置需要生成的表名
+                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                })
+//                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+}
